4b0f2ba23989cc06e1b9f517c5ec65e8da879e80
Paras821 <samdhiyaparas21@gmail.com>
2024-12-10 16:13:40 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,56 @@
+for x in range(1,50):
+    if x == 4:
+        continue
+    if x == 6:
+        continue
+    if x == 7:
+        continue
+    if x == 9:
+        continue
+    if x == 10:
+        continue
+    if x == 11:
+        continue
+    if x == 12:
+        continue
+    if x == 14:
+        continue
+    if x == 15:
+        continue
+    if x == 16:
+        continue
+    if x == 17:
+        continue
+    if x == 18:
+        continue
+    if x == 19:
+        continue
+    if x == 20:
+        continue
+    if x == 22:
+        continue
+    if x == 23:
+        continue
+    if x == 24:
+        continue
+    if x == 25:
+        continue
+    if x == 26:
+        continue
+    if x == 27:
+        continue
+    if x == 28:
+        continue
+    if x == 29:
+        continue
+    if x == 30:
+        continue
+    if x == 31:
+        continue
+    if x == 32:
+        continue
+    if x == 33:
+        continue
+    if x == 35:
+       break
+    print(x)
---
+++ assignment3.py
@@ -0,0 +1,5 @@
+x = int(input("Enter Your Number \n"))
+print(x)
+
+x = x * (x-1)
+print(x)
