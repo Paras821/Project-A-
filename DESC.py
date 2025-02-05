8b11e8c93fa789a4ae25566b10a6cb012b1d6494
Paras Samdhiya <samdhiyaparas21@gmail.com>
2025-02-05 14:23:01 +0530

    Add files via upload

---
+++ DESC.py
@@ -0,0 +1,16 @@
+
+import mysql.connector
+
+conn = mysql.connector.MySQLConnection(host="localhost", user="root", password="Root@123", database="mydatabase")
+
+mycursor = conn.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name DESC"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
+
